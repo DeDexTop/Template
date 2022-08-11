3f81c874d8c4d623c1c4080a9c2ed4c8c3bc6ac5
Arief Aliman <4r1efaliman@gmail.com>
2022-08-11 13:57:13 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Template
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelDesktopPanel = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnNotification = new System.Windows.Forms.Button();
+            this.btnReporting = new System.Windows.Forms.Button();
+            this.btnCostumer = new System.Windows.Forms.Button();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnProducts = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.btnSettings);
+            this.panelMenu.Controls.Add(this.btnNotification);
+            this.panelMenu.Controls.Add(this.btnReporting);
+            this.panelMenu.Controls.Add(this.btnCostumer);
+            this.panelMenu.Controls.Add(this.btnOrder);
+            this.panelMenu.Controls.Add(this.btnProducts);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(235, 603);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(85, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MENU";
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelTitleBar.Controls.Add(this.button1);
+            this.panelTitleBar.Controls.Add(this.labelTitle);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(235, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(863, 80);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.White;
+            this.labelTitle.Location = new System.Drawing.Point(370, 27);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(112, 37);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "HOME";
+            // 
+            // panelDesktopPanel
+            // 
+            this.panelDesktopPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktopPanel.Location = new System.Drawing.Point(235, 80);
+            this.panelDesktopPanel.Name = "panelDesktopPanel";
+            this.panelDesktopPanel.Size = new System.Drawing.Size(863, 523);
+            this.panelDesktopPanel.TabIndex = 2;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelLogo.Controls.Add(this.button2);
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(235, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Template.Properties.Resources.menu_burger_free_icon_font__1_;
+            this.button1.Location = new System.Drawing.Point(21, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 48);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSettings.Image = global::Template.Properties.Resources.settings_free_icon_font;
+            this.btnSettings.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSettings.Location = new System.Drawing.Point(0, 380);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnSettings.Size = new System.Drawing.Size(235, 60);
+            this.btnSettings.TabIndex = 6;
+            this.btnSettings.Text = "   Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // btnNotification
+            // 
+            this.btnNotification.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNotification.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNotification.FlatAppearance.BorderSize = 0;
+            this.btnNotification.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNotification.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNotification.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnNotification.Image = global::Template.Properties.Resources.bell_free_icon_font;
+            this.btnNotification.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNotification.Location = new System.Drawing.Point(0, 320);
+            this.btnNotification.Name = "btnNotification";
+            this.btnNotification.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnNotification.Size = new System.Drawing.Size(235, 60);
+            this.btnNotification.TabIndex = 5;
+            this.btnNotification.Text = "   Notification";
+            this.btnNotification.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNotification.UseVisualStyleBackColor = true;
+            this.btnNotification.Click += new System.EventHandler(this.btnNotification_Click);
+            // 
+            // btnReporting
+            // 
+            this.btnReporting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReporting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReporting.FlatAppearance.BorderSize = 0;
+            this.btnReporting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporting.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReporting.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnReporting.Image = global::Template.Properties.Resources.chart_histogram_free_icon_font;
+            this.btnReporting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReporting.Location = new System.Drawing.Point(0, 260);
+            this.btnReporting.Name = "btnReporting";
+            this.btnReporting.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnReporting.Size = new System.Drawing.Size(235, 60);
+            this.btnReporting.TabIndex = 4;
+            this.btnReporting.Text = "   Reporting";
+            this.btnReporting.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReporting.UseVisualStyleBackColor = true;
+            this.btnReporting.Click += new System.EventHandler(this.btnReporting_Click);
+            // 
+            // btnCostumer
+            // 
+            this.btnCostumer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCostumer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCostumer.FlatAppearance.BorderSize = 0;
+            this.btnCostumer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCostumer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCostumer.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCostumer.Image = global::Template.Properties.Resources.hand_holding_box_free_icon_font;
+            this.btnCostumer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCostumer.Location = new System.Drawing.Point(0, 200);
+            this.btnCostumer.Name = "btnCostumer";
+            this.btnCostumer.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnCostumer.Size = new System.Drawing.Size(235, 60);
+            this.btnCostumer.TabIndex = 3;
+            this.btnCostumer.Text = "   Constumer";
+            this.btnCostumer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCostumer.UseVisualStyleBackColor = true;
+            this.btnCostumer.Click += new System.EventHandler(this.btnCostumer_Click);
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOrder.FlatAppearance.BorderSize = 0;
+            this.btnOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrder.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnOrder.Image = global::Template.Properties.Resources.clipboard_list_free_icon_font;
+            this.btnOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrder.Location = new System.Drawing.Point(0, 140);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnOrder.Size = new System.Drawing.Size(235, 60);
+            this.btnOrder.TabIndex = 2;
+            this.btnOrder.Text = "   Order";
+            this.btnOrder.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProducts.FlatAppearance.BorderSize = 0;
+            this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProducts.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnProducts.Image = global::Template.Properties.Resources.shopping_cart_free_icon_font;
+            this.btnProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.Location = new System.Drawing.Point(0, 80);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnProducts.Size = new System.Drawing.Size(235, 60);
+            this.btnProducts.TabIndex = 1;
+            this.btnProducts.Text = "   Products";
+            this.btnProducts.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProducts.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProducts.UseVisualStyleBackColor = true;
+            this.btnProducts.Click += new System.EventHandler(this.btnProducts_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::Template.Properties.Resources.angle_left_free_icon_font;
+            this.button2.Location = new System.Drawing.Point(0, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 48);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 603);
+            this.Controls.Add(this.panelDesktopPanel);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.MinimumSize = new System.Drawing.Size(1000, 559);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnProducts;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnNotification;
+        private System.Windows.Forms.Button btnReporting;
+        private System.Windows.Forms.Button btnCostumer;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelDesktopPanel;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Template
+{
+    public partial class Form1 : Form
+    {
+        //Fields
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activateForm;
+
+        //Constuctor
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        //Method
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while(tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+        public static Color ChangeColorBrightness(Color color, double correctionFactor)
+        {
+            double red = color.R;
+            double green = color.G;
+            double blue = color.B;
+            //If correction factor is less than 0, darken color.
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+            //If correction factor is greater than zero, lighten color.
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+            return Color.FromArgb(color.A, (byte)red, (byte)green, (byte)blue);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if(btnSender != null)
+            {
+                if(currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+
+                    Color color = Color.FromArgb(51, 51, 76);
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = ChangeColorBrightness(color, -0.3);
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelLogo.BackColor = Color.Transparent;
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            foreach(Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activateForm != null)
+            {
+                activateForm.Close();
+            }
+            ActivateButton(btnSender);
+            activateForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPanel.Controls.Add(childForm);
+            this.panelDesktopPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            labelTitle.Text = childForm.Text;
+        }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Product(), sender);
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Order(), sender);
+        }
+
+        private void btnCostumer_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Costumer(), sender);
+        }
+
+        private void btnReporting_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Reporting(), sender);
+        }
+
+        private void btnNotification_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Notification(), sender);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.Settings(), sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            panelMenu.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            panelMenu.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            panelMenu.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            panelMenu.Visible = false;
+            button2.Visible = false;
+        }
+    }
+}
